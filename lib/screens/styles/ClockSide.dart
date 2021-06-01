3ab7bdbfa3e88d9d01af42665e7e1d971e937b89
Raúl Peñate <raul.h.penate@gmail.com>
2021-06-01 06:39:48 -0600

    :lipstick: [ADD] ClockSide colors

---
+++ lib/screens/styles/ClockSide.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//WhiteSide
+const WhiteSideColor = Color(0xFF034694);
+const WhiteSideRotation = RotatedBox(quarterTurns: 2);
+
+//BlackSide
+const BlackSideColor = Color(0xFF6CABDD);
+const BlackSideRotation = RotatedBox(quarterTurns: 4);
+
+//Clock Running colors
+const ClockRunningColorBox = Color(0xFF6CABDD);
+const ClockRuningColorText = Color(0xFFFFFFFF);
