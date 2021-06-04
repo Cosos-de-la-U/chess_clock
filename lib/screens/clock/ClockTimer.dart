c30944511366a5c1925bf15e186f86e865cbff40
Raúl Peñate <raul.h.penate@gmail.com>
2021-06-04 07:09:30 -0600

    :iphone: [ADD] Responsive en ClockTimer

--- lib/screens/clock/ClockTimer.dart
+++ lib/screens/clock/ClockTimer.dart
@@ -1,4 +1,4 @@
-import 'package:chess_timer/screens/components/PauseButton.dart';
+import 'package:chess_timer/screens/components/ButtonsInTheMiddle.dart';
 import 'package:chess_timer/screens/components/TimerBox.dart';
 import 'package:chess_timer/screens/styles/ClockSide.dart';
 import 'package:flutter/material.dart';
@@ -11,33 +11,24 @@ class ClockTimer extends StatefulWidget {
 class _ClockTimerState extends State<ClockTimer> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        body: Stack(
-          children: [
-            SafeArea(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                //Creating timer for white side
-                Flexible(child: TimerBox(WhiteSideColor, WhiteSideRotation)),
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              //Creating timer for white side
+              Flexible(child: TimerBox(WhiteSideColor, WhiteSideRotation),flex: 12,),
 
-                //Creating buttons in the middle
-                Container(
-                  child: Column(
-                    children: [
-                      
-                      PauseButton(),
-                    ],
-                  ),
-                ),
+              //Creating buttons in the middle
+              Flexible(child: SizedBox(child: ButtonsInTheMiddle(),), flex: 1,),
 
-                //Creating timer for black side
-                Flexible(child: TimerBox(BlackSideColor, BlackSideRotation)),
-              ],
-            ))
-          ],
-        ),
+              //Creating timer for black side
+              Flexible(child: TimerBox(BlackSideColor, BlackSideRotation), flex: 12,),
+            ],
+          ))
+        ],
       ),
     );
   }
