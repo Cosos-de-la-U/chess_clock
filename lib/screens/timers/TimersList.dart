02a5c03e1b2dc9dd7569852679a8902ea2aa2134
Raúl Peñate <raul.h.penate@gmail.com>
2021-06-08 23:07:02 -0600

    :sparkles: [ADD] Tilelist to TimerList

--- lib/screens/timers/TimersList.dart
+++ lib/screens/timers/TimersList.dart
@@ -1,3 +1,4 @@
+import 'package:chess_timer/screens/clock/ClockTimer.dart';
 import 'package:chess_timer/screens/components/TitleAppBar.dart';
 import 'package:flutter/material.dart';
 
@@ -11,15 +12,36 @@ class TimersList extends StatefulWidget {
 class _TimersListState extends State<TimersList> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: TitleAppBar(),
+          centerTitle: true,
           actions: [
             IconButton(onPressed: () => {}, icon: Icon(Icons.settings))
           ],
         ),
-      ),
-    );
+        floatingActionButton:
+            FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
+        body: Stack(
+          children: [
+            SafeArea(
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.library_add_check_sharp),
+                    title: Text("1 + 1"),
+                    subtitle: Text("Bullet, no berserk"),
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => ClockTimer())),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.library_add_check_sharp),
+                    title: Text("10 + 0"),
+                    subtitle: Text("Rapid, no berserk"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
